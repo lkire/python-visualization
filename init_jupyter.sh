d01dac19d25cb5d87e566306ff45d6d65b072794
lkire <alkire@gmail.com>
2019-01-01 00:01:19 +0000

    jupyter setup

---
+++ init_jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#jupyter notebook --generate-config
+
+#this is the only one we need actually.
+jupyter notebook password
+
+#ipython
+#from notebook.auth import passwd
+#passwd()
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#suggest jsut running through these commands instead of running script
+
+#conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+sudo apt update
+sudo apt install libgl1-mesa-glx
+
+conda install msgpack-python sqlAlchemy tensorflow=1.11.0 keras scikit-learn numpy scipy matplotlib pandas holoviews geoviews bokeh datashader param jupyter
+
+rm Miniconda3-latest-Linux-x86_64.sh
