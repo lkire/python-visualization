3cec2204d39b19611b615d971173cc83eceecd9a
lkire <alkire@gmail.com>
2019-01-23 15:02:42 +0000

    initial seven colors

---
+++ colors.py
@@ -0,0 +1,9 @@
+
+website01 =['#757575', #react gray
+			'#328CC1', #ryptco light blue
+			'#103C5D', #ryptco dark blue
+			'#D26C2C', #open weather orange
+			'#75436B', #bokeh purple
+			'#D98310', #ryptco gold
+			'#A83122', #oasis red
+]
